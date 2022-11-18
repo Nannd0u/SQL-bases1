b49fdf9703bef4ad9adb9ef490ced948d3c69039
Luis Diaz <luisnando710@protonmail.com>
2022-11-18 13:44:52 -0400

    feat: atributos desde variedad hasta padrino

--- create.sql
+++ create.sql
@@ -1,10 +1,47 @@
-create table Variedad();
-create table Cultivo();
-create table Produccion();
-create table Asociacion();
-create table Productor();
-create table Proveedor();
-create table Padrino();
+create table Variedad(
+    var_id
+    var_nom
+    var_maduracion
+    var_paisorigen
+    var_desc
+);
+create table Cultivo(
+    cult_id
+    cult_calibre
+    cult_prodanual
+    cult_periodoini
+    cult_periodofin
+    cult_destexport
+);
+create table Produccion(
+    prod_id
+    prod_year
+    prod_logradakg
+);
+create table Asociacion(
+    asoc_id
+    asoc_nom
+    asoc_dir
+);
+create table Productor(
+    p_id
+    p_nom
+    p_dir
+    p_envase
+);
+create table Proveedor(
+    prov_id
+    prov_nom
+    prov_negocio
+    prov_dir
+);
+create table Padrino(
+    pad_id
+    pad_nom
+    pad_seg_nom
+    pad_ape
+    pad_seg_ape
+);
 create table Apadrinamiento();
 create table Converion();
 create table Ciudad();
