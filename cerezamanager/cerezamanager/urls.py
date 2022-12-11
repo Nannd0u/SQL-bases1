16744cde77f07be7be7ad18b6fa4de7014753afe
Nannd0u <luisnando710@protonmail.com>
2022-12-11 14:30:24 -0400

    feat: index and urls from apps to settings.py

--- cerezamanager/cerezamanager/urls.py
+++ cerezamanager/cerezamanager/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('transaccion/', include('transaccion.urls')),
+    path('productores/', include('productores.urls')),
+    path('evaluacion/', include('evaluacion.urls')),
+    path('recetas/', include('recetas.urls')),
 ]
