f3dc8bd324bd588a160f31f3302c196ef55541db
Nannd0u <luisnando710@protonmail.com>
2022-12-13 00:20:23 -0400

    feat: views and templates for app evaluacion

--- cerezamanager/evaluacion/urls.py
+++ cerezamanager/evaluacion/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'evaluacion'
 urlpatterns = [
+    # ex: /evaluacion/
     path('', views.index, name='index'),
+    # ex: /evaluacion/evaluar/
+    path('<int:productor_id>/', views.detalle, name='detalle'),
+    # ex: /evaluacion/5/form/
+    path('<int:productor_id>/form.html/', views.form, name='form'),
+    # ex: /evaluacion/5/resultados/
+    path('<int:productor_id>/resultados.html/', views.resultados, name='resultados'),
 ]
--- cerezamanager/evaluacion/views.py
+++ cerezamanager/evaluacion/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from productores.models import Productor
 from django.http import HttpResponse
+from .models import ResultadoEvalAnual
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the evaluacion index.")
+    latest_productor_list = Productor.objects.all()
+    return render(request, 'evaluacion/index.html', {'latest_productor_list': latest_productor_list})
+
+# pagina que muestra los productores para evaluar
+def detalle(request, productor_id):
+    productor = get_object_or_404(Productor, pk=productor_id)
+    return render(request, 'evaluacion/detalle.html', {'productor': productor})
+
+# pagina que muestra el cuestionario para evaluar
+def form(request, productor_id):
+    productor = get_object_or_404(Productor, pk=productor_id)
+    return render(request, 'evaluacion/form.html', {'productor': productor})
+
+# pagina que muestra los resultados de evaluaciones de un productor
+def resultados(request, rea_id):
+    return HttpResponse("You're looking at the results of question %s." % rea_id)
+    """rea = get_object_or_404(ResultadoEvalAnual, pk=rea_id)
+    try:
+        selected_choice = rea.choice_set.get(pk=request.POST['choice'])
+    except (KeyError):"""
